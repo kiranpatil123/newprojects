a7d1405cfbe6897507b15e2e8123ed3166bface3
kiran <kiranpatil123>
2019-07-18 22:06:13 +0530

    First commit.

---
+++ demogenerics/src/com/kiran/Demo.java
@@ -0,0 +1,21 @@
+package com.kiran;
+
+class Add<T> {
+
+	@SuppressWarnings("unchecked")
+	public T add(int a, int b, Class<T> t) {
+		int c = a + b;
+		Integer sc = new Integer(c);
+		return (T) sc.toString();
+	}
+}
+
+public class Demo {
+	public static void main(String[] args) {
+		Add a = new Add();
+		Object add = a.add(5, 6, Integer.class);
+		System.out.println(add);
+		System.out.println("kiran");
+	}
+
+}
